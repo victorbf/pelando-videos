8a656d99e464948c3b06c0d1ac535f010edd797a
victorbf <victorbeiraof@gmail.com>
2020-03-05 19:32:11 -0300

    Create button styles

---
+++ ui/src/assets/styles/global/Variables.style.js
@@ -0,0 +1,15 @@
+export const LightGray = '#E2E8F0';
+export const LightBlack = '#333333';
+export const LightGreen = '#99CC33';
+export const DarkYellow = '#FFCC00';
+export const DarkRed = '#CC3300';
+export const DarkOrange = '#CC6100';
+export const White = '#FFFFFF';
+
+// Color hierarchy
+export const FontColor = LightBlack;
+export const PrimaryColor = DarkOrange;
+export const SecondaryColor = LightGray;
+export const DangerColor = DarkRed;
+export const WarningColor = DarkYellow;
+export const SuccessColor = LightGreen;
---
+++ ui/src/components/common/Button.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import {
+  PrimaryColor, SecondaryColor, White,
+  DangerColor, WarningColor, SuccessColor,
+} from '../../assets/styles/global/Variables.style';
+
+const currentBGColor = (props) => {
+  switch (props) {
+    case 'primary':
+      return PrimaryColor;
+    case 'danger':
+      return DangerColor;
+    case 'warning':
+      return WarningColor;
+    case 'success':
+      return SuccessColor;
+    default:
+      return SecondaryColor;
+  }
+};
+
+const Button = styled.button`
+  background: ${(props) => currentBGColor(props.action)};
+  color: ${(props) => props.action && White};
+  padding: 0.5rem 2rem;
+  border: 1px solid ${SecondaryColor};
+`;
+
+export default Button;
