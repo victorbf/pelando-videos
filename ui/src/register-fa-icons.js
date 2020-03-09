d88f47adbc8c787db2374ccf9cd86c08ca2f66fa
victorbf <victorbeiraof@gmail.com>
2020-03-09 02:45:11 -0300

    css and js config

---
+++ ui/src/register-fa-icons.js
@@ -0,0 +1,16 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faPause,
+  faPlay,
+  faTimes,
+  faPlus,
+  faMinus,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faPause,
+  faPlay,
+  faTimes,
+  faPlus,
+  faMinus,
+);
