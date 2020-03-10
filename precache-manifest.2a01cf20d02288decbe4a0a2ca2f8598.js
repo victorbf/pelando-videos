51683f33faff9a81d0c44bd766479ceee29b3c76
victorbf <victorbeiraof@gmail.com>
2020-03-09 22:18:44 -0300

    Updates

---
+++ precache-manifest.2a01cf20d02288decbe4a0a2ca2f8598.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4ec9b5e1c9525d365d0d96f2e3f0a4",
+    "url": "/pelando-videos/index.html"
+  },
+  {
+    "revision": "a24974bc25c66d63e8f0",
+    "url": "/pelando-videos/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "945eddf66ac522fe7070",
+    "url": "/pelando-videos/static/css/main.92dcd69c.chunk.css"
+  },
+  {
+    "revision": "a24974bc25c66d63e8f0",
+    "url": "/pelando-videos/static/js/2.479116a6.chunk.js"
+  },
+  {
+    "revision": "96dffc2c7df3cb0bb38ecf67be59e8c5",
+    "url": "/pelando-videos/static/js/2.479116a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "945eddf66ac522fe7070",
+    "url": "/pelando-videos/static/js/main.37b843d7.chunk.js"
+  },
+  {
+    "revision": "729de6cc5e9507661970",
+    "url": "/pelando-videos/static/js/runtime-main.20a059b1.js"
+  },
+  {
+    "revision": "87658c72f79a1c95a657c74e9ac23974",
+    "url": "/pelando-videos/static/media/pelando-icon.87658c72.png"
+  },
+  {
+    "revision": "b49d27aaf81ffd64150fb4c82bfd4d2b",
+    "url": "/pelando-videos/static/media/pelando.b49d27aa.png"
+  }
+]);
